9fc8744e06fb68f4f1769558ee8fe05390d3bb79
Princeyyyy <princemumo80@gmail.com>
2020-10-13 00:44:33 +0300

    added the encrypt and decrypt functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,70 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args){
+
+        Scanner aSCANNER = new Scanner(System.in);
+        String plainText;
+        int shiftKey;
+
+        System.out.println("Please enter messsage to be encrypted: ");
+        plainText = aSCANNER.nextLine();
+
+        System.out.println("Please enter Shift Key; ");
+        shiftKey = aSCANNER.nextInt();
+        String encrypedText = EncryptText(plainText,shiftKey);
+
+
+        System.out.println("The Encrypted Text: "+encrypedText);
+        System.out.println("The Decrypted Text: "+DecryptText(encrypedText,shiftKey));
+    }
+
+    public static String EncryptText(String message, int shiftKey){
+        final String ALPHABETS = "abcdefghijklmopqrstuvwxyz";
+        message = message.toLowerCase();
+
+        StringBuilder cypherText = new StringBuilder();
+
+        for (int counter = 0;counter < message.length();counter++){
+            if (!Character.isLetter(message.charAt(counter))){
+                cypherText.append(message.charAt(counter));
+            }
+            else {
+                int charPosition = ALPHABETS.indexOf(message.charAt(counter));
+                int keyValue = (charPosition + shiftKey) % 26;
+
+                char cyperValue = ALPHABETS.charAt(keyValue);
+
+                cypherText.append(cyperValue);
+            }
+        }
+        return cypherText.toString();
+    }
+
+    public static String DecryptText(String message, int shiftKey){
+        final String ALPHABETS = "abcdefghijklmopqrstuvwxyz";
+        message = message.toLowerCase();
+
+        StringBuilder plainText = new StringBuilder();
+
+        for (int counter = 0;counter < message.length();counter++){
+            if (!Character.isLetter(message.charAt(counter))){
+                plainText.append(message.charAt(counter));
+            }
+            else {
+                int charPosition = ALPHABETS.indexOf(message.charAt(counter));
+                int keyValue = (charPosition - shiftKey) % 26;
+
+                if (keyValue < 0){
+                    keyValue = ALPHABETS.length() + keyValue;
+                }
+
+                char plainValue = ALPHABETS.charAt(keyValue);
+
+                plainText.append(plainValue);
+            }
+        }
+        return plainText.toString();
+    }
 }
+
